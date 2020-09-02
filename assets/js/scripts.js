f5d6273c88635510d4d26f9056116c7da184bae0
Marc McHardy <marc@marcmchardy.com>
2020-09-02 21:32:04 +1200

    ADD: Touch event tracking for logo, kiwiruby content, header spiel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,16 +25,34 @@ connected = false,
 snapDist = 180,
 currentEl = 0,
 multiplier = 0,
-posArray = [];
+posArray = [],
+snapAllowed = true,
+freshTouchdown = false;
 
 letters.forEach( function(el, index) {
     posArray[index] = {x: 0, y: 0,  offsetX: offset(el).left, offsetY: offset(el).top, width: offset(el).right - offset(el).left, height: offset(el).bottom - offset(el).top  };
 });
 
-document.addEventListener("mousemove", onMove);
+document.addEventListener("touchstart", function(){
+    freshTouchdown = true;
+});
+document.addEventListener("touchmove", function(event){
+    onMove(event, "touch")
+});
+document.addEventListener("mousemove", function(event){
+    onMove(event, "mouse")
+});
 
 gsap.ticker.add(update);
 update();
+
+function snapAllowedTimer(){
+    snapAllowed = false;
+    freshTouchdown = false;
+    setTimeout(function(){ 
+        snapAllowed = true; 
+    }, 250);
+}
 
 function update() {
     letters.forEach( function(el, index){
@@ -60,28 +78,38 @@ function update() {
         if (Math.abs(posArray[currentEl].y) > snapDist || Math.abs(posArray[currentEl].x) > snapDist*1.5) {   
             // or if we are not the current el but we're not at startY and need to animate back
             connected = false;
+            snapAllowedTimer();
+
             gsap.to( posArray[index] , { 
                 duration: 2,  
                 x: 0,
                 y: 0
             });
         }  
-
     });
-    
 }
 
-function onMove(event) {
+var inputPos = [{x:0, y:0}];
+
+function onMove(event, input) {
+
+    // override to keep the touch check from breaking the mouse interaction
+    freshTouchdown = (input == "touch") ? freshTouchdown : true;
+
     letters.forEach( function(el, index) {
-        if (!connected && event.target === el) {  
+        if (!connected && snapAllowed && freshTouchdown && event.target === el) {  
             connected = true;    
             currentEl = index;
          }
          if (connected && index == currentEl) {    
              // Kill any active tweens on the point
             gsap.killTweensOf(posArray[currentEl]); 
-            posArray[currentEl].y = (event.pageY - (posArray[currentEl].offsetY + (posArray[currentEl].height / 2)))*1.5 ; 
-            posArray[currentEl].x = (event.pageX - (posArray[currentEl].offsetX + (posArray[currentEl].width / 2)))*1.5 ; 
+
+            inputPos.x = (input == "mouse") ? event.pageX : (input == "touch") ? event.touches[0].pageX : '';
+            inputPos.y = (input == "mouse") ? event.pageY : (input == "touch") ? event.touches[0].pageY : '';
+
+            posArray[currentEl].y = (inputPos.y - (posArray[currentEl].offsetY + (posArray[currentEl].height / 2)))*1.5 ; 
+            posArray[currentEl].x = (inputPos.x - (posArray[currentEl].offsetX + (posArray[currentEl].width / 2)))*1.5 ; 
         }
     });
 }
